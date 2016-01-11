5e13e91824f0a61495736f77d5658a89918bd20a
Derek Dik <1upderek@gmail.com>
2016-01-11 21:13:04 +0900

    Implemented weapon firing animation and burst fire mode

--- Assets/Scripts/ShootScriptAssaultRifle.cs
+++ Assets/Scripts/ShootScriptAssaultRifle.cs
@@ -7,33 +7,61 @@ public class ShootScriptAssaultRifle : MonoBehaviour
 
     // Rocket Prefab
     public GameObject rocketPrefab;
+    private Animator rifleAnimator;
+
+    void Start()
+    {
+        // Get the rifle animator
+        rifleAnimator = GameObject.Find("CQAssaultRifle").GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        // left mouse clicked?
-        if (Input.GetMouseButtonDown(0))
+        // left mouse clicked? rifle firing animation finished?
+        if (Input.GetMouseButtonDown(0) && !rifleAnimator.GetBool("Firing"))
         {
-            // spawn rocket
-            // - Instantiate means 'throw the prefab into the game world'
-            // - (GameObject) cast is required because unity is stupid
-            // - transform.position because we want to instantiate it exactly
-            //   where the weapon is
-            // - transform.parent.rotation because the rocket should face the
-            //   same direction that the player faces (which is the weapon's
-            //   parent.
-            //   we can't just use the weapon's rotation because the weapon is
-            //   always rotated like 45° which would make the bullet fly really
-            //   weird
-            GameObject g = (GameObject)Instantiate(rocketPrefab,
-                                                   transform.position,
-                                                   transform.parent.rotation);
+            // Toggle the firing animation
+            rifleAnimator.SetBool("Firing", true);
 
-            // make the rocket fly forward by simply calling the rigidbody's
-            // AddForce method
-            // (requires the rocket to have a rigidbody attached to it)
-            float force = g.GetComponent<BulletController>().speed;
-            g.GetComponent<Rigidbody>().AddForce(g.transform.forward * force);
+            // Burst fire
+            StartCoroutine(Burst());
+            
         }
+    }
+
+    // Asynchronous method to fire three shots
+    private IEnumerator Burst()
+    {
+        // Fire three times            
+        Fire();
+        yield return new WaitForSeconds(0.05f);
+        Fire();
+        yield return new WaitForSeconds(0.05f);
+        Fire();
+    }
+
+    private void Fire()
+    {
+        // spawn rocket
+        // - Instantiate means 'throw the prefab into the game world'
+        // - (GameObject) cast is required because unity is stupid
+        // - transform.position because we want to instantiate it exactly
+        //   where the weapon is
+        // - transform.parent.rotation because the rocket should face the
+        //   same direction that the player faces (which is the weapon's
+        //   parent.
+        //   we can't just use the weapon's rotation because the weapon is
+        //   always rotated like 45° which would make the bullet fly really
+        //   weird
+        GameObject g = (GameObject)Instantiate(rocketPrefab,
+                                               transform.position,
+                                               transform.parent.rotation);
+
+        // make the rocket fly forward by simply calling the rigidbody's
+        // AddForce method
+        // (requires the rocket to have a rigidbody attached to it)
+        float force = g.GetComponent<BulletController>().speed;
+        g.GetComponent<Rigidbody>().AddForce(g.transform.forward * force);
     }
 }
