13330fb817c60de5efbb6d89b33f4474be26f60c
Derek Dik <1upderek@gmail.com>
2016-01-13 10:49:53 +0900

    Rotating Level

--- Assets/Scripts/LevelRotate.cs
+++ Assets/Scripts/LevelRotate.cs
@@ -8,7 +8,7 @@ public class LevelRotate : MonoBehaviour {
     private float velocity = 0;
     private float currentAngle;
     private float startRotationAngle;
-    private float iterator = 0;
+    private float velocity_iterator = 0;
     // Use this for initialization
 	void Start () {
         currentAngle = transform.rotation.z;
@@ -18,26 +18,40 @@ public class LevelRotate : MonoBehaviour {
     void beginRotation()
     {
         startRotationAngle = currentAngle;
+        isRotating = true;
     }
 
 	// Update is called once per frame
 	void Update () {
-        currentAngle = transform.eulerAngles.z;
+        // If this level should currently rotate:
         if(isRotating){
+            // Get the current angle of rotation
+            currentAngle = transform.eulerAngles.z;
+            // Check edge case: Make sure that the angle hasn't crossed 359 degrees and reverted to 0
+            if (currentAngle == 0 && startRotationAngle == 270)
+            {
+                velocity = 0;
+                isRotating = false;
+            }
+            // If the rotation is less than halfway complete, increase the velocity by one
             if (currentAngle < startRotationAngle + 45)
             {
-                iterator += 1.0f * rotationSpeed;
+                velocity_iterator += 1.0f * rotationSpeed;
             }
-            else if (currentAngle < startRotationAngle + 90)
+            // If the rotation is more than halfway complete but not complete, decrease the velocity by one
+            else if (currentAngle > startRotationAngle && currentAngle < startRotationAngle + 90)
             {
-                iterator -= 1.0f * rotationSpeed;            }
+                velocity_iterator -= 1.0f * rotationSpeed;            }
+            // If rotation is complete, set velocity to 0 and set the bool flag isRotating to false to end rotation
             else
             {
                 velocity = 0;
-                beginRotation();
+                isRotating = false;
 
             }
-            velocity = iterator * iterator * rotationSpeed;
+            // Square the velocity
+            velocity = velocity_iterator * velocity_iterator * rotationSpeed;
+            // Rotate
             transform.eulerAngles = new Vector3(0, 0, currentAngle + (velocity * rotationSpeed));
 
         }
