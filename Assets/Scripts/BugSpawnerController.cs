b84ce8921fe34a6966213e6e4d26c5b496cce741
Derek Dik <1upderek@gmail.com>
2016-01-15 17:01:10 +0900

    Added basic enemy with spawner

---
+++ Assets/Scripts/BugSpawnerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BugSpawnerController : MonoBehaviour {
+    public GameObject bugPrefab;
+    public GameObject target;
+    public float minTimeBetweenRotations = 5.0f;
+    public float maxTimeBetweenRotations = 30.0f;
+    private bool isWaiting = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isWaiting)
+        {
+            isWaiting = true;
+            StartCoroutine(TimedSpawn());
+        }
+    }
+
+    private IEnumerator TimedSpawn()
+    {
+        float timeBetweenRotations = Random.Range(minTimeBetweenRotations, maxTimeBetweenRotations);
+        yield return new WaitForSeconds(timeBetweenRotations);
+        spawn();
+        isWaiting = false;
+    }
+
+    private void spawn()
+    {
+        GameObject g = (GameObject)Instantiate(bugPrefab,
+                                               transform.position,
+                                               transform.parent.rotation);
+        g.GetComponent<BugFollowController>().target = target;
+    }
+}
---
+++ Assets/Scripts/DestructibleObject.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestructibleObject : MonoBehaviour
+{
+    public GameObject self;
+    public float health = 25.0f;
+    public GameObject destructionParticlePrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (health > 0.0f && other.gameObject.name != self.name)
+        {
+            health -= 5.0f;
+        }
+        else
+        {
+            die();
+        }
+
+
+    }
+
+    private void die()
+    {
+        GameObject g = (GameObject)Instantiate(destructionParticlePrefab,
+                                                       transform.position,
+                                                       Quaternion.identity);
+        Destroy(self);
+        Debug.Log("Destructible object has been destroyed.");
+    }
+
+}
